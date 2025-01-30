071de26acd31cfdacea714b1cfc7b8091ec2767f
francisihe <francis.ihejirika@gmail.com>
2025-01-30 22:39:50 +0100

    feat: setup cors to use allowedDomains only

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { NODE_ENV, PORT } from './config/constants';
 import connectToDatabase from './config/db/connectToDB';
+import { corsErrorHandler, corsMiddleware } from './middleware/corsErrorHandler';
 
 const app = express();
 app.use(express.json());
 
+// CORS middleware, to allow only domains in config/allowedDomains.ts
+app.use(corsMiddleware);
+app.use(corsErrorHandler);
 
 const startServer = async () => {
     try {
